eaeccd2d1a2c2292870be94d8812c4c647dc436d
H. Watanabe <kaityo@users.sourceforge.jp>
2025-07-16 16:29:07 +0900

    :memo: updates

--- analyze_font_sizes.rb
+++ analyze_font_sizes.rb
@@ -1,25 +1,11 @@
-#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# !/usr/bin/env ruby
 require 'bundler/setup'
 require 'hexapdf'
 require 'hexapdf/content/parser'
 
-def show_stream(doc)
-  doc.each do |obj|
-    next unless obj.is_a?(HexaPDF::Stream)
-
-    str = obj.stream
-    next unless str.ascii_only?
-
-    puts '----------------------------------------------------'
-    puts str
-  end
-end
-
-def count_utf16be_chars(byte_string)
-  byte_string.bytes.each_slice(2).count
-end
-
-def analyze(doc)
+def analyze(doc, verbose)
   parser = HexaPDF::Content::Parser.new
 
   doc.pages.each_with_index do |page, _page_index|
@@ -32,6 +18,8 @@ def analyze(doc)
     contents = page.contents
     contents = [contents] unless contents.is_a?(Array)
 
+    font_size_hash = Hash.new(0)
+    current_size = 0
     contents.each do |stream|
       stream = doc.object(stream) if stream.is_a?(Integer)
       buffer = stream.respond_to?(:stream) ? stream.stream : stream
@@ -40,19 +28,28 @@ def analyze(doc)
         case operator
         when :Tf
           current_font, current_size = operands
-          puts "#{current_font} #{current_size}"
+          puts "#{current_font} #{current_size}" if verbose
         when :TJ
-          array = operands[0]
-          text = array.join.to_s
-          puts text if text.ascii_only?
+          operands[0].each do |v|
+            next unless v.is_a?(String)
+
+            if v.ascii_only?
+              font_size_hash[current_size] += v.size
+            else
+              font_size_hash[current_size] += v.size / 2
+              v = "<#{v.bytes.map { |b| format('%02X', b) }.join}>" unless v.ascii_only?
+            end
+            puts v if verbose
+          end
         end
       end
     end
+    p font_size_hash
   end
 end
 
 filename = ARGV[0]
 HexaPDF::Document.open(filename) do |doc|
   # show_stream(doc)
-  analyze(doc)
+  analyze(doc, true)
 end
